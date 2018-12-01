0d08aa6f0cce3b99110dc0c6ebb4cde7b2a9b84e
Rodrigo Antunes <ro_sgca@hotmail.com>
2018-12-01 16:13:06 -0200

    serviço de upload movido para o serviço de livros

--- LivrariaVirtual/src/main/java/br/senac/livrariavirtual/servicos/LivroServico.java
+++ LivrariaVirtual/src/main/java/br/senac/livrariavirtual/servicos/LivroServico.java
@@ -1,5 +1,10 @@
 package br.senac.livrariavirtual.servicos;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -10,7 +15,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import br.senac.livrariavirtual.modelo.Livro;
 import br.senac.livrariavirtual.modelo.Usuario;
@@ -130,6 +140,34 @@ public class LivroServico {
 			System.out.println(ex.getMessage());
 			return false;
 		}	
+	}
+	
+	@POST
+	@Path("/UploadLivro")
+	@Consumes({MediaType.MULTIPART_FORM_DATA})
+	public Response uploadPdfFile(  @FormDataParam("file") InputStream fileInputStream,
+	                                @FormDataParam("file") FormDataContentDisposition fileMetaData) throws Exception
+	{
+		//home/aluno/Dev/upload/arquivos/
+		//System.out.print(System.getProperty("user.dir"));
+	    String UPLOAD_PATH = "/home/aluno/Área de Trabalho/Dev/Repositorio/PI-6---BackEnd/LivrariaVirtual/arquivos/";
+	    try
+	    {
+	        int read = 0;
+	        byte[] bytes = new byte[1024];
+	 
+	        OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileMetaData.getFileName()));
+	        while ((read = fileInputStream.read(bytes)) != -1)
+	        {
+	            out.write(bytes, 0, read);
+	        }
+	        out.flush();
+	        out.close();
+	    } catch (IOException e)
+	    {
+	        throw new WebApplicationException("Error while uploading file. Please try again !!");
+	    }
+	    return Response.ok("PDF adicionado com sucesso !!").build();
 	}
 	
 	
